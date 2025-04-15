cf8af19680823d8407343986dc9b4fb7f9b04c6c
sobannaeem01 <soban.naeem764@gmail.com>
2025-04-15 18:58:44 +0500

    login screen ui completed
    - password
    - eye toggle
    -align password & eye

--- App.tsx
+++ App.tsx
@@ -1,24 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { View, Image, StyleSheet, Alert, TouchableOpacity, Text, TextInput } from 'react-native';
-  
+
 const LogoFromURL = () => {
   const [email, setEmail] = useState('');
-    const [heading1] = useState('GET STARTED NOW!');
-    const [heading2] = useState('Enter Your email and get stated right\naway.');
+  const [password, setpassword] = useState('')
+  const [isPasswordVisible, setIsPasswordVisible] = useState(true);
+
+
+  const [heading1] = useState('GET STARTED NOW!');
+  const [heading2] = useState('Enter Your email and get stated right\naway.');
 
 
 
-    useEffect(() => {
-      console.log("Fahad useeffect Initial: ", email);
-    }, []);
+  useEffect(() => {
+    console.log("Fahad useeffect Initial: ", email);
+  }, []);
 
-    useEffect(() => {
-      console.log("Fahad useeffect called: ", email);
-    }, [email]);
+  useEffect(() => {
+    console.log("Fahad useeffect called: ", email);
+  }, [email]);
 
-    useEffect(() => {
-      console.log("Fahad useeffect heading: ", email);
-    }, [heading1]);
+  useEffect(() => {
+    console.log("Fahad useeffect heading: ", email);
+  }, [heading1]);
 
 
 
@@ -35,11 +39,11 @@ const LogoFromURL = () => {
         style={styles.logo}
       /> */}
 
-<Image
+      <Image
         source={require("./src/assets/googleLogo.png")}
         style={styles.logo}
       />
-      
+
       <View style={styles.textContainer}>
         <Text style={styles.textBold}>{heading1}</Text>
       </View>
@@ -48,20 +52,54 @@ const LogoFromURL = () => {
       </View>
 
 
-     
+
       <View style={styles.inputContainer}>
 
-      <Text style={styles.textEmail}>Email:</Text>
-      
+        <Text style={styles.textEmail}>Email:</Text>
+
 
         <TextInput
           style={styles.input}
           placeholder="Enter your email"
           value={email}
           onChangeText={setEmail}
-          keyboardType="email-address"  // Keyboard for email input
-          autoCapitalize="none"         // Prevents auto-capitalization
+          // Prevents auto-capitalization
+          autoCapitalize='none'
+          keyboardType="email-address"
         />
+      </View>
+
+      <View style={styles.inputContainer2}>
+
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.textpassword}>Password:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter Your Password"
+            value={password}
+            onChangeText={setpassword}
+
+            secureTextEntry={isPasswordVisible} // Hide password when false
+            autoCapitalize='none'
+            keyboardType='default'
+
+
+          />
+        </View>
+
+
+        <TouchableOpacity
+          onPress={() => {
+            setIsPasswordVisible(!isPasswordVisible);
+          }} style={styles.emojiButton}>
+            
+          <Text style={styles.emojiText}>
+            {isPasswordVisible ? '🙈' : '👁️'} {/* Emoji toggles */}
+          </Text>
+        </TouchableOpacity>
+
+
       </View>
 
       <View style={styles.buttonContainer}>
@@ -70,6 +108,7 @@ const LogoFromURL = () => {
         </TouchableOpacity>
       </View>
     </View>
+
   );
 };
 
@@ -104,7 +143,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 
-  
+
   textContainer: {
     backgroundColor: 'fafafa',
     padding: 5,
@@ -130,6 +169,7 @@ const styles = StyleSheet.create({
     fontSize: 15,
     color: 'black',
     textAlign: "center"
+
   },
 
   //email text 
@@ -137,27 +177,56 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'light',
     color: 'black',
-     //backgroundColor: 'orange',
+    //backgroundColor: 'orange',
     textAlign: "left",
 
   },
+  //Password text 
+  textpassword: {
+    fontSize: 20,
+    fontWeight: 'light',
+    color: 'black',
+    textAlign: "left",
 
-  // Email input field
-  inputContainer: {
-    marginTop: 200,
-    width: '80%',
-    // backgroundColor: 'red',
   },
-  
+  // email input field
+  inputContainer: {
+
+    width: '80%',
+    //backgroundColor: 'red',
+
+  },
+
+
+  //password input field
+  inputContainer2: {
+    marginTop: 10,
+    width: '80%',
+    flexDirection: 'row',
+    alignItems: 'flex-end'
+  },
+  emojiButton: {
+    padding: 8,
+    flexDirection: 'row'
+  },
+  emojiText: {
+    fontSize: 20,
+  },
+  //emoji 
+
+
+
   input: {
-    marginLeft:2,
+    marginLeft: 2,
     height: 40,
     borderColor: '#ccc',
     borderWidth: 1,
     borderRadius: 5,
     paddingLeft: 10,
     backgroundColor: '#fff',
+    color: "black"
   },
+
 });
 
 export default LogoFromURL;
